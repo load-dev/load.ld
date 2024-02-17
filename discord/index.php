334611ffeb23c8a0f17eb130e3ded25e30e0cb4e
LOAD <122114788+load-dev@users.noreply.github.com>
2024-02-17 16:06:50 +0000

    Update website

---
+++ discord/index.php
@@ -0,0 +1,5 @@
+<?php
+$redirectUrl = 'https://discord.gg/DTZm4hepnA';
+header('Location: ' . $redirectUrl);
+exit();
+?>
---
+++ twitter/index.php
@@ -0,0 +1,5 @@
+<?php
+$redirectUrl = '/';
+header('Location: ' . $redirectUrl);
+exit();
+?>
